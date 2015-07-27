0c04a141f9cf0336ce6435d79d6c83ccb630caf8
Lucas do Amaral Saboya <lucas.saboya@gmail.com>
2015-07-27 19:07:40 -0300

    modificada a ordem de instalacao

--- .osx/set-defaults.sh
+++ .osx/set-defaults.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Ask for the administrator password upfront
 echo ".osx scripts"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
