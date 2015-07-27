6115f8fe0e21baca5e8831d2a90d3af87efa54ea
Lucas do Amaral Saboya <lucas.saboya@gmail.com>
2015-07-27 14:28:37 -0300

    Adicionando parametros especificos pro megasync, lastpass e little snitch installer; adicionado evernote cask

--- .homebrew/install.sh
+++ .homebrew/install.sh
@@ -40,7 +40,11 @@ brew install wget --with-iri
 # Install homebrew cask
 brew tap caskroom/cask
 brew install brew-cask
-brew cask install atom google-chrome spotify alfred 1password slack the-unarchiver lastpass little-snitch cleanmymac cinch teamviewer iterm2 megasync libreoffice transmissio emby-server
+brew cask install atom google-chrome spotify alfred 1password slack the-unarchiver lastpass little-snitch cleanmymac cinch teamviewer iterm2 libreoffice transmissio emby-server evernote
+brew cask --appdir=/Applications install megasync
+open -a /opt/homebrew-cask/Caskroom/little-snitch/3.5.3/Little\ Snitch\ Installer.app
+open -a /opt/homebrew-cask/Caskroom/lastpass/latest/LastPass\ Installer.app/
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
