9c0329a3a9a347391711d32964c72403320e050d
Lucas do Amaral Saboya <lucas.saboya@gmail.com>
2015-07-28 17:49:39 -0300

    Modificados os passos para instalacao do oh-my-zsh

--- .zshrc/install.sh
+++ .zshrc/install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ln -s ${BASEDIR}/.zshrc/.zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
