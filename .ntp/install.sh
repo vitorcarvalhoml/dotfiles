a74ce7b3262ea59737556c9c62ece1ecd0f712ff
Lucas do Amaral Saboya <lucas.saboya@gmail.com>
2015-07-28 19:27:36 -0300

    Adicionadas configurações de NTP

---
+++ .ntp/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "server a.ntp.br" > /etc/ntp.conf
+echo "server b.ntp.br" >> /etc/ntp.conf
+echo "server c.ntp.br" >> /etc/ntp.conf
+echo "server gps.ntp.br" >> /etc/ntp.conf
+echo "server a.st1.ntp.br" >> /etc/ntp.conf
+echo "server b.st1.ntp.br" >> /etc/ntp.conf
+echo "server c.st1.ntp.br" >> /etc/ntp.conf
+echo "server d.st1.ntp.br" >> /etc/ntp.conf
