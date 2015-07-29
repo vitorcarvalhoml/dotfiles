9452907b0c178d83f6686caabd8ffc751fc2dc63
Lucas do Amaral Saboya <lucas.saboya@gmail.com>
2015-07-29 19:29:28 -0300

    adicionado sudo aos comandos de configuracao do ntp

--- .ntp/install.sh
+++ .ntp/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Ask for the administrator password upfront.
+sudo -v
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 echo "server a.ntp.br" > /etc/ntp.conf
 echo "server b.ntp.br" >> /etc/ntp.conf
 echo "server c.ntp.br" >> /etc/ntp.conf
