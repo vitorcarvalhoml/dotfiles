e45d783dfef511094c4bb76bf11732a80b9b47cb
Lucas do Amaral Saboya <lucas.saboya@gmail.com>
2015-07-29 19:31:50 -0300

    adicionado sudo aos comandos de configuracao do ntp

--- .ntp/install.sh
+++ .ntp/install.sh
@@ -3,11 +3,11 @@
 sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-echo "server a.ntp.br" > /etc/ntp.conf
-echo "server b.ntp.br" >> /etc/ntp.conf
-echo "server c.ntp.br" >> /etc/ntp.conf
-echo "server gps.ntp.br" >> /etc/ntp.conf
-echo "server a.st1.ntp.br" >> /etc/ntp.conf
-echo "server b.st1.ntp.br" >> /etc/ntp.conf
-echo "server c.st1.ntp.br" >> /etc/ntp.conf
-echo "server d.st1.ntp.br" >> /etc/ntp.conf
+sudo echo "server a.ntp.br" > /etc/ntp.conf
+sudo echo "server b.ntp.br" >> /etc/ntp.conf
+sudo echo "server c.ntp.br" >> /etc/ntp.conf
+sudo echo "server gps.ntp.br" >> /etc/ntp.conf
+sudo echo "server a.st1.ntp.br" >> /etc/ntp.conf
+sudo echo "server b.st1.ntp.br" >> /etc/ntp.conf
+sudo echo "server c.st1.ntp.br" >> /etc/ntp.conf
+sudo echo "server d.st1.ntp.br" >> /etc/ntp.conf
