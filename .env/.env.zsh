0aec810757538f0b8119fbb01b30c77faf58569a
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2015-09-20 19:47:58 -0300

    adding latex binaries

--- .env/.env.zsh
+++ .env/.env.zsh
@@ -1,6 +1,6 @@
 echo ".env scripts"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/texbin"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export VAGRANT_DEFAULT_PROVIDER=virtualbox
