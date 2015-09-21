ba69c8dec0079dbef51ebb2ab263d753abc91d2a
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2015-09-21 00:23:28 -0300

    adding npm dotfiles

--- .env/.env.zsh
+++ .env/.env.zsh
@@ -1,6 +1,9 @@
 echo ".env scripts"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/texbin"
+export NPM_PACKAGES="${HOME}/.npm-packages"
+export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$NPM_PACKAGES/bin:/usr/texbin:"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export VAGRANT_DEFAULT_PROVIDER=virtualbox
