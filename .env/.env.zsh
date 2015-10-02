23f17f80e02bf9953b9030d84e34976376cdcf48
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2015-10-02 10:10:58 -0300

    organizing PATH variable

--- .env/.env.zsh
+++ .env/.env.zsh
@@ -2,8 +2,9 @@ echo ".env scripts"
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 export NPM_PACKAGES="${HOME}/.npm-packages"
 export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
+export LATEX_PATH=/usr/texbin
 
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$NPM_PACKAGES/bin:/usr/texbin:"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$NPM_PACKAGES/bin:$LATEX_PATH:"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export VAGRANT_DEFAULT_PROVIDER=virtualbox
