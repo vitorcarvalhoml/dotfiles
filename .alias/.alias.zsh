61a83bb06723259ac22599972e900a797c4e3ebe
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2016-01-26 18:58:17 -0300

    adding st3 configs

--- .alias/.alias.zsh
+++ .alias/.alias.zsh
@@ -1,6 +1,2 @@
-# List all files colorized in long format
-# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
-alias update='sudo softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup'
-
-# Flush Directory Service cache
-alias dnsflush="dscacheutil -flushcache && killall -HUP mDNSResponder"
+# Vi to VIM
+alias vim="vi" 
