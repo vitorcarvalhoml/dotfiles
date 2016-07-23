f7b3f6cb37bf528dcb99565b622fdda776c70edb
Vitor Carvalho <vitorcarvalhoml@gmail.com>
2016-07-22 23:40:38 -0300

    adding projects to sublime

--- .env/.env.zsh
+++ .env/.env.zsh
@@ -3,6 +3,7 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
 export NPM_PACKAGES="${HOME}/.npm-packages"
 export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
 export LATEX_PATH=/usr/local/texlive/2015/bin/x86_64-darwin
+export HOMEBREW_GITHUB_API_TOKEN="87c13c747adc00ade971a611e83588f7ae9be260"
 
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$NPM_PACKAGES/bin:$LATEX_PATH:"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
